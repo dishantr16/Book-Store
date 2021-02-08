d0453e85c4e8ee7f4b0dd416ff8d771a8899962c
Dishant Rathi <dishant16rathi@gmail.com>
2021-02-08 20:50:33 +0530

    Image fields added and Urls are set

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
-    in_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
